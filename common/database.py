77118d1989e33ec507bdc6d96965d201bea7c66a
Borga Usifo <borga.usifo@gmail.com>
2019-12-07 14:10:51 -0600

    MongoDB connection as well as common request from DB

--- common/database.py
+++ common/database.py
@@ -1,7 +1,6 @@
 from typing import Dict
 import pymongo
 
-
 class Database:
     DATABASE = None
 
@@ -13,3 +12,24 @@ class Database:
     @staticmethod
     def insert(collection: str, data: Dict):
         Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection: str, query: Dict) -> pymongo.cursor:
+        """ Returns the cursor which something to iterate over."""
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection: str, query: Dict) -> Dict:
+        """ Returns the dictionary """
+        return Database.DATABASE[collection.find_one(query)]
+
+    @staticmethod
+    def update(collection: str, query: Dict, data: Dict) -> None:
+        Database.DATABASE[collection].update(query, data, upsert = True)
+
+    @staticmethod
+    def remove(colletion: str, query: Dict) -> Dict:
+        Database.DATABASE[colletion].remove(query)
+
+
+
