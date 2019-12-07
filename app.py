ecfcc9b17933359617b14ab5239960718f68782a
Borga Usifo <borga.usifo@gmail.com>
2019-12-07 15:53:11 -0600

    Routes added

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from flask import Flask
+import json
+from flask import Flask, render_template, request
 from learning import learning_blueprint
 from models.item import Item
 
-url = 'https://www.johnlewis.com/asus-chromebook-flip-c433ta-intel-core-m3-4gb-ram-64gb-emmc-14-inch-full-hd-silver/p4811855'
-tag_name = 'p'
-query = {'class': 'price price--large'}
+app = Flask(__name__)
 
-asus_chrome = Item(url, tag_name, query)
-asus_chrome.save_to_mongo()
 
-items_loaded = Item.all()
-print(items_loaded[0].load_price())
+@app.route('/', methods=['GET', 'POST'])
+def new_item():
+    if request.method == 'POST':
+        url = request.form['url']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        Item(url, tag_name, query).save_to_mongo()
+
+    return render_template('new_item.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# url = 'https://www.johnlewis.com/asus-chromebook-flip-c433ta-intel-core-m3-4gb-ram-64gb-emmc-14-inch-full-hd-silver/p4811855'
+# tag_name = 'p'
+# query = {'class': 'price price--large'}
+#
+# asus_chrome = Item(url, tag_name, query)
+# asus_chrome.save_to_mongo()
+#
+# items_loaded = Item.all()
+# print(items_loaded[0].load_price())
 
 # app = Flask(__name__)
 #
