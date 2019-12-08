7c3cc7b58f34fe44272066e05902d8e77c901c96
Borga Usifo <borga.usifo@gmail.com>
2019-12-08 12:20:47 -0600

    Changing the route and adding blueprint for items

--- app.py
+++ app.py
@@ -2,21 +2,10 @@ import json
 from flask import Flask, render_template, request
 from learning import learning_blueprint
 from models.item import Item
+from views.items import item_blueprint
 
 app = Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def new_item():
-    if request.method == 'POST':
-        url = request.form['url']
-        tag_name = request.form['tag_name']
-        query = json.loads(request.form['query'])
-
-        Item(url, tag_name, query).save_to_mongo()
-
-    return render_template('new_item.html')
-
+app.register_blueprint(item_blueprint, url_prefix='/items')
 
 if __name__ == '__main__':
     app.run(debug=True)
